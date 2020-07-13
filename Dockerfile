279e6f680904b54e0a059c1f1c2c6f747556762c
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-07-13 15:33:18 -0600

    nri api init

--- Dockerfile
+++ Dockerfile
@@ -5,5 +5,6 @@ RUN npm install
 COPY . /app
 CMD node index.js 
 EXPOSE 5010
+
 # docker image build -t nri_api .
 # docker container run --rm -d --name nri -p 5010:5010 nri_api
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 if(process.env.NODE_ENV!=='production'){
   require('dotenv').config()
 }
-const express = require('express')
+const express = require('express') 
+
 
 const app = express()
 const db = require('./config/database')
